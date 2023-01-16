2008553d637f159c8f74b9f169f2f3a6565af780
aSpired92 <maciejkobiela92@gmail.com>
2023-01-16 01:03:46 +0100

    Tests passed. Added resolver

--- openapi/resolvers.py
+++ openapi/resolvers.py
@@ -1,30 +1,15 @@
+import json
 import os
-import typing
 
-from fastapi.openapi import models
-
-from openapi.loaders import DocumentLoader
+import jsonref
 
 
-class Ref:
+def _loader(uri):
+    # custom ref resolving
+    return jsonref.jsonloader(uri)
 
-    def __init__(self, ref: str, referrer_path: str):
-        self.ref = ref
-        self.referrer = referrer_path
 
-    def resolve(self) -> models.Schema:
-        ref_parts = self.ref.split('#')
-        temp_path = self.referrer
-        if ref_parts[0]:
-            for module in ref_parts[0].split('/'):
-                temp_path = os.path.join(temp_path, module)
-        current_object = DocumentLoader.load(temp_path)
-        for module in ref_parts[1].split('/'):
-            if module:
-                if type(current_object) == dict:
-                    current_object = current_object.get(module)
-                else:
-                    current_object = getattr(current_object, module)
-        print(current_object)
-        return current_object
+def resolve(uri):
+    with open(uri) as fp:
+        return jsonref.load(fp, loader=_loader)
 
