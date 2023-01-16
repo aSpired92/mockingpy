b6d122f46fbb19e1b72649b312e99bd331e17068
aSpired92 <maciejkobiela92@gmail.com>
2023-01-16 01:14:23 +0100

    Added resolver support (works way better)

--- generator/app.py
+++ generator/app.py
@@ -14,17 +14,6 @@ from fastapi.openapi import models
 from openapi import loaders
 
 
-NO_DESCRIPTION = 'No description'
-
-
-def get_ref_name(ref):
-    return str(ref).split('/')[-1]
-
-
-def get_model(ref_nam):
-    return getattr(response_models, ref_nam)
-
-
 class Dynamic:
 
     @classmethod
@@ -50,12 +39,9 @@ async def response_data({query_params}):
 
 
 api = FastAPI()
-doc_path = os.path.join(config.MAIN_DIR, 'openapi.yaml')
+doc_path = os.path.join(config.MAIN_DIR, 'openapi.json')
 document = loaders.DocumentLoader.load(doc_path)
 
-model_generator.generate(input_=Path(doc_path), input_file_type=model_generator.InputFileType.OpenAPI, output=Path(config.MODELS_DIR))
-
-response_models = importlib.import_module('dynamic.models')
 
 paths = document.paths
 
@@ -75,45 +61,17 @@ for path in paths:
     for method in methods:
         method_object = methods.get(method)
         if method_object:
-            # Dynamic().add_endpoint(method_object.parameters)
+            Dynamic().add_endpoint(method_object.parameters)
+
+            responses = {response: method_object.responses.get(response).dict() for response in method_object.responses}
 
             params = {
                 'path': path,
                 'endpoint': getattr(Dynamic, 'response_data'),
                 'methods': [method],
                 'description': method_object.description,
-                'operation_id': method_object.operationId
-
+                'operation_id': method_object.operationId,
+                'responses': responses
             }
-
-            responses = {}
-
-            for response in method_object.responses:
-                response_object = method_object.responses.get(response)
-                new_response = {
-                    'description': response_object.description,
-                }
-
-                if response_object.content:
-                    new_content = {}
-                    for content in response_object.content:
-                        new_content_type = {}
-                        content_object = response_object.content.get(content)
-                        if content_object.example:
-                            new_content_type['example'] = content_object.example
-                            new_content[content] = new_content_type
-                        schema = content_object.schema_
-
-                        if schema.items:
-                            ref_name = get_ref_name(schema.items.ref)
-                            new_response['model'] = List[get_model(ref_name)]
-                        elif schema.ref:
-                            new_response['model'] = get_model(get_ref_name(schema.ref))
-
-                    new_response['content'] = new_content
-
-                responses[response] = new_response
-
-            params['responses'] = responses
 
             api.add_api_route(**params)
