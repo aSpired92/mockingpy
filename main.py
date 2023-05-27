3654e96743ef24bd995a9546acd6eabe21c9ecc7
Maciej Kobiela <maciejkobiela92@gmail.com>
2023-05-27 21:42:31 +0000

    Modified gitignore on new machine

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ import uvicorn
 
 if __name__ == "__main__":
     # Dla możliwości rozbudowy podstawowych argumentów uvicorn jest uruchamiany programowo
-    uvicorn.run("generator.app:api", host="0.0.0.0", port=8055, log_level="debug", reload=True)
+    uvicorn.run("generator.app:api", host="0.0.0.0",
+                port=8055, log_level="debug", reload=True)
