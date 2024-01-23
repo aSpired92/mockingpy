2e1a48977bbe370ae2277b62df97fe35eeaacba6
aSpired92 <maciejkobiela92@gmail.com>
2024-01-23 03:55:39 +0100

    Update loaders.py

--- openapi/loaders.py
+++ openapi/loaders.py
@@ -1,16 +1,17 @@
 from fastapi.openapi import models
 from openapi_spec_validator import validate_spec
 
+import args
 from openapi import resolvers
 
 
-def load_document(document_path):
+def load_document():
     """
-    Zaciąga i waliduje dokumentację, rozwiązuje wszystkie referencje
+    Pulls and validates documentation, resolves all references
+    :return: Returns a documentation object
+    """
 
-    :param document_path: Ścieżka do pliku
-    :return: Zwraca obiekt dokumentacji
-    """
+    document_path = args.document_path
     data = resolvers.resolve_refs(document_path)
     validate_spec(data)
     data = models.OpenAPI(**data)
