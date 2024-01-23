2c68e6160cda20833de1288f72ca73da1699df24
aSpired92 <maciejkobiela92@gmail.com>
2024-01-23 03:56:15 +0100

    Update loggers.py

--- loggers.py
+++ loggers.py
@@ -1,5 +1,12 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+import args
+
+if args.log_level:
+    level = args.log_level.upper()
+else:
+    level = logging.INFO
+
+logging.basicConfig(level=level)
 main_logger = logging.getLogger("uvicorn")
-main_logger.setLevel(logging.DEBUG)
+main_logger.setLevel(level)
